50fc1d1d18c8eeb4e58629263f5e8a08515deced
MurodilNorboev <your_email@example.com>
2024-12-12 13:49:16 +0900

    first commit

---
+++ aa/aaTests/aaTests.swift
@@ -0,0 +1,17 @@
+//
+//  aaTests.swift
+//  aaTests
+//
+//  Created by Macbook  on 12/12/24.
+//
+
+import Testing
+@testable import aa
+
+struct aaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
